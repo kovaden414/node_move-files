d5b7c8724393baaf75db146e19afa87601a165d2
Denys Kovalchuk <denkov414@gmail.com>
2025-04-05 23:11:12 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-// write code here
+/* eslint-disable no-console */
+const fsp = require('fs/promises');
+const path = require('path');
+
+async function main() {
+  const oldPath = process.argv[2];
+  let newPath = process.argv[3];
+
+  try {
+    const destStat = await fsp.stat(newPath);
+
+    if (destStat.isDirectory()) {
+      const filename = path.basename(oldPath);
+
+      newPath = path.join(newPath, filename);
+    }
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      console.error(error);
+    }
+  }
+
+  if (oldPath !== newPath) {
+    try {
+      await fsp.rename(oldPath, newPath);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+main();
